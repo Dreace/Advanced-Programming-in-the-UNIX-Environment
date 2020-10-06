e703fd411d71cf12fddaa03ce06bd66bb345b1df
Dreace <Dreace@Foxmail.com>
2020-10-06 21:32:00 +0800

    2020.10.9

--- main.c
+++ main.c
@@ -1,35 +1,72 @@
 #include "apue.h"
+#include <sys/resource.h>
 
-#define BSZ 48
+#define doit(name) pr_limits(#name, name)
+
+static void pr_limits(char *, int);
 
 int
-main() {
-    FILE *fp;
-    char buf[BSZ];
-    memset(buf, 'a', BSZ - 2);
-    buf[BSZ - 2] = '\0';
-    buf[BSZ - 1] = 'X';
-    if ((fp = fmemopen(buf, BSZ, "w+")) == NULL)
-        err_sys("fmemopen failed");
-    printf("initial buffer contents: %s\n", buf);
-    fprintf(fp, "hello, world");
-    printf("before flush: %s\n", buf);
-    fflush(fp);
-    printf("after fflush: %s\n", buf);
-    printf("len of string in buf = %ld\n", (long) strlen(buf));
-    memset(buf, 'b', BSZ - 2);
-    buf[BSZ - 2] = '\0';
-    buf[BSZ - 1] = 'X';
-    fprintf(fp, "hello, world");
-    fseek(fp, 0, SEEK_SET);
-    printf("after fseek: %s\n", buf);
-    printf("len of string in buf = %ld\n", (long) strlen(buf));
-    memset(buf, 'c', BSZ - 2);
-    buf[BSZ - 2] = '\0';
-    buf[BSZ - 1] = 'X';
-    fprintf(fp, "hello, world");
-    fclose(fp);
-    printf("after fclose: %s\n", buf);
-    printf("len of string in buf = %ld\n", (long) strlen(buf));
-    return (0);
+main(void) {
+#ifdef RLIMIT_AS
+    doit(RLIMIT_AS);
+#endif
+    doit(RLIMIT_CORE);
+    doit(RLIMIT_CPU);
+    doit(RLIMIT_DATA);
+    doit(RLIMIT_FSIZE);
+#ifdef RLIMIT_MEMLOCK
+    doit(RLIMIT_MEMLOCK);
+#endif
+#ifdef RLIMIT_MSGQUEUE
+    doit(RLIMIT_MSGQUEUE);
+#endif
+#ifdef RLIMIT_NICE
+    doit(RLIMIT_NICE);
+#endif
+    doit(RLIMIT_NOFILE);
+#ifdef RLIMIT_NPROC
+    doit(RLIMIT_NPROC);
+#endif
+#ifdef RLIMIT_NPTS
+    doit(RLIMIT_NPTS);
+#endif
+#ifdef RLIMIT_RSS
+    doit(RLIMIT_RSS);
+#endif
+#ifdef RLIMIT_SBSIZE
+    doit(RLIMIT_SBSIZE);
+#endif
+#ifdef RLIMIT_SIGPENDING
+    doit(RLIMIT_SIGPENDING);
+#endif
+    doit(RLIMIT_STACK);
+#ifdef RLIMIT_SWAP
+    doit(RLIMIT_SWAP);
+#endif
+#ifdef RLIMIT_VMEM
+    doit(RLIMIT_VMEM);
+#endif
+    exit(0);
+}
+
+static void
+pr_limits(char *name, int resource) {
+    struct rlimit limit;
+    unsigned long long lim;
+    if (getrlimit(resource, &limit) < 0)
+        err_sys("getrlimit error for %s", name);
+    printf("%-14s ", name);
+    if (limit.rlim_cur == RLIM_INFINITY) {
+        printf("(infinite) ");
+    } else {
+        lim = limit.rlim_cur;
+        printf("%10lld ", lim);
+    }
+    if (limit.rlim_max == RLIM_INFINITY) {
+        printf("(infinite)");
+    } else {
+        lim = limit.rlim_max;
+        printf("%10lld", lim);
+    }
+    putchar((int) '\n');
 }
